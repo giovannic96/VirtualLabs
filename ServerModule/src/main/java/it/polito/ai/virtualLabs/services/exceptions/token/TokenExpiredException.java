ad5a3e93a9a34c1389b166220d62f88dcca77c6a
giovannic96 <giovanni.cala@hotmail.it>
2020-07-19 16:21:46 +0200

    removed tokens

--- ServerModule/src/main/java/it/polito/ai/virtualLabs/services/exceptions/token/TokenExpiredException.java
+++
@@ -1,9 +0,0 @@
-package it.polito.ai.virtualLabs.services.exceptions.token;
-
-import it.polito.ai.virtualLabs.services.exceptions.notification.NotificationServiceException;
-
-public class TokenExpiredException extends NotificationServiceException {
-    public TokenExpiredException(String message) {
-        super(message + " Error type: TOKEN EXPIRED");
-    }
-}
--- ServerModule/src/main/java/it/polito/ai/virtualLabs/services/exceptions/token/TokenNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package it.polito.ai.virtualLabs.services.exceptions.token;
-
-import it.polito.ai.virtualLabs.services.exceptions.notification.NotificationServiceException;
-
-public class TokenNotFoundException extends NotificationServiceException {
-    public TokenNotFoundException(String message) {
-        super(message + " Error type: TOKEN NOT FOUND");
-    }
-}
