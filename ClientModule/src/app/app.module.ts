c4498fd8b0ad849b8ca4c7f3a8a81dcfed621f4e
giovannic96 <giovanni.cala@hotmail.it>
2020-08-22 19:47:21 +0200

    add versions to Labs tab

--- ClientModule/src/app/app.module.ts
+++ ClientModule/src/app/app.module.ts
@@ -16,31 +16,33 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StudentsComponent } from './components/tabs/students/students.component';
 import { RouterModule } from '@angular/router';
-import {routes} from './app-routing-module';
-import {HomeComponent} from './components/main/home/home.component';
-import {PersonalComponent} from './components/main/personal/personal.component';
-import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
-import {MatDialogModule} from '@angular/material/dialog';
-import {LoginDialogComponent} from './auth/login-dialog.component';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {AuthInterceptorService} from './auth/auth-interceptor.service';
+import { routes } from './app-routing-module';
+import { HomeComponent } from './components/main/home/home.component';
+import { PersonalComponent } from './components/main/personal/personal.component';
+import { HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
+import { MatDialogModule } from '@angular/material/dialog';
+import { LoginDialogComponent } from './auth/login-dialog.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AuthInterceptorService } from './auth/auth-interceptor.service';
 import { VmComponent } from './components/tabs/vms/vm.component';
 import { LabComponent } from './components/tabs/labs/lab.component';
 import { TeamComponent } from './components/tabs/teams/team.component';
 import { CourseInfoComponent } from './components/tabs/info/course-info.component';
 import { SignupDialogComponent } from './auth/signup-dialog.component';
 import { ProfileComponent } from './auth/profile.component';
-import {PageNotFoundComponent} from './components/main/pagenotfound.component';
-import {MatExpansionModule} from '@angular/material/expansion';
-import {MatSnackBarModule} from '@angular/material/snack-bar';
-import {MySnackBarComponent} from './helpers/my-snack-bar.component';
-import {MatCardModule} from '@angular/material/card';
-import {MatSelectModule} from '@angular/material/select';
-import {MatSliderModule} from '@angular/material/slider';
+import { PageNotFoundComponent } from './components/main/pagenotfound.component';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MySnackBarComponent } from './helpers/my-snack-bar.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSliderModule } from '@angular/material/slider';
 import { VmModelSettingsDialogComponent } from './components/tabs/vms/vm-model-settings-dialog.component';
 import { MyDialogComponent } from './helpers/my-dialog.component';
-import {EmailDialogComponent} from './helpers/email-dialog.component';
-import {MatChipsModule} from "@angular/material/chips";
+import { EmailDialogComponent } from './helpers/email-dialog.component';
+import { VersionDialogComponent } from './helpers/version-dialog.component';
+import { MatChipsModule } from '@angular/material/chips';
+import { ExtendedModule, FlexLayoutModule, FlexModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
@@ -59,7 +61,8 @@ import {MatChipsModule} from "@angular/material/chips";
     MySnackBarComponent,
     VmModelSettingsDialogComponent,
     MyDialogComponent,
-    EmailDialogComponent
+    EmailDialogComponent,
+    VersionDialogComponent,
   ],
   imports: [
     RouterModule.forRoot(routes, {enableTracing: false}),
@@ -87,6 +90,9 @@ import {MatChipsModule} from "@angular/material/chips";
     MatSelectModule,
     MatSliderModule,
     MatChipsModule,
+    FlexLayoutModule,
+    FlexModule,
+    ExtendedModule,
   ],
   exports: [
     RouterModule,
@@ -100,6 +106,6 @@ import {MatChipsModule} from "@angular/material/chips";
     }
   ],
   bootstrap: [AppComponent],
-  entryComponents: [LoginDialogComponent]
+  entryComponents: [LoginDialogComponent, EmailDialogComponent, VersionDialogComponent]
 })
 export class AppModule { }
--- ClientModule/src/app/components/tabs/labs/lab.component.ts
+++ ClientModule/src/app/components/tabs/labs/lab.component.ts
@@ -6,6 +6,11 @@ import {Observable} from 'rxjs';
 import {Course} from '../../../models/course.model';
 import {LabService} from '../../../services/lab.service';
 import {Report, ReportStatus} from '../../../models/report.model';
+import {Student} from "../../../models/student.model";
+import {MatDialog, MatDialogConfig} from "@angular/material/dialog";
+import {EmailDialogComponent} from "../../../helpers/email-dialog.component";
+import {MessageType} from "../../../helpers/my-snack-bar.component";
+import {VersionDialogComponent} from "../../../helpers/version-dialog.component";
 
 @Component({
   selector: 'app-lab',
@@ -16,9 +21,11 @@ export class LabComponent implements OnInit {
 
   private currentCourse: Observable<Course>;
   public assignmentList: Assignment[];
+  public gridColumns = 3;
 
   constructor(private courseService: CourseService,
-              private labService: LabService) {
+              private labService: LabService,
+              private dialog: MatDialog) {
 
     this.currentCourse = this.courseService.getSelectedCourse().pipe(filter(course => !!course));
 
@@ -62,6 +69,34 @@ export class LabComponent implements OnInit {
     return new Date(splitted[0], splitted[1] - 1, splitted[2], splitted[3], splitted[4], splitted[5]);
   }
 
+  openVersionDialog(versionTitle: string, versionContent: string) {
+    const dialogConfig = new MatDialogConfig();
+
+    dialogConfig.disableClose = true;
+    dialogConfig.autoFocus = true;
+
+    dialogConfig.data = {
+      title: versionTitle,
+      content: versionContent
+    };
+
+    const dialogRef = this.dialog.open(VersionDialogComponent, dialogConfig);
+
+    dialogRef.afterClosed().subscribe(
+      data => {
+        if (data !== undefined) { // i.e. close button was pressed
+          /*
+          this.notificationService.sendMessage(emails, data.subject, data.body).subscribe( () => {
+            this.mySnackBar.openSnackBar('Email sent successfully', MessageType.SUCCESS, 3);
+          }, () => {
+            this.mySnackBar.openSnackBar('Error while sending the email. Some students may not have received the email correctly', MessageType.ERROR, 3);
+          });
+           */
+        }
+      }
+    );
+  }
+
   getColorForStatus(status: string) {
     switch (status) {
       case ReportStatus.NULL: {
@@ -80,5 +115,9 @@ export class LabComponent implements OnInit {
         return '#000000';
       }
     }
+  }
+
+  toggleGridColumns() {
+    this.gridColumns = this.gridColumns === 3 ? 4 : 3;
   }
 }
