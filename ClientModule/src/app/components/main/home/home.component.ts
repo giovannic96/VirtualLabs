b767a4830cb0c79761c063d7db3e7b7e7262d6d1
dariopatti90 <dario.patti90@gmail.com>
2020-09-07 20:36:08 +0200

    link homepage download button to the executable

--- ClientModule/src/app/components/main/home/home.component.ts
+++ ClientModule/src/app/components/main/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {CourseService} from '../../../services/course.service';
+import {AuthService} from '../../../services/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -8,9 +9,12 @@ import {CourseService} from '../../../services/course.service';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private courseService: CourseService) {
-    this.courseService.hideMenu.next(false);
-    this.courseService.hideMenuIcon.next(true);
+  desktopAppLink = 'https://virtuallabs.ns0.it/installers/VirtualLabs-x64-setup.exe';
+
+  constructor(private authService: AuthService,
+              private courseService: CourseService) {
+              this.courseService.hideMenu.next(false);
+              this.courseService.hideMenuIcon.next(true);
   }
 
   ngOnInit(): void {
