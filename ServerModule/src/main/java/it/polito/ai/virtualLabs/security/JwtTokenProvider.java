87ec0d9f8086868c2314f81f7e16bd958edfed44
dariopatti90 <dario.patti90@gmail.com>
2020-10-25 22:15:29 +0100

    correct auth token expiry time

--- ServerModule/src/main/java/it/polito/ai/virtualLabs/security/JwtTokenProvider.java
+++ ServerModule/src/main/java/it/polito/ai/virtualLabs/security/JwtTokenProvider.java
@@ -21,8 +21,8 @@ public class JwtTokenProvider {
     @Value("${security.jwt.token.secret-key:secret}")
     private String secretKey = "secret";
 
-    @Value("${security.jwt.token.expire-length:10000}")
-    private long validityInMilliseconds = 10000; // 1h
+    @Value("${security.jwt.token.expire-length:3600000}")
+    private long validityInMilliseconds = 3600000; // 1h
 
     @Autowired
     private UserDetailsService userDetailsService;
